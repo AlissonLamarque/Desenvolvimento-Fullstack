f6c4bdc9c4b1b4aa48fd124fc51ee88c9c34e2ee
Laboratório <laboratorio@ACAD.UFN.EDU.BR>
2025-05-21 20:12:45 -0300

    Ajustes front

--- vet-clinic-front/src/App.js
+++ vet-clinic-front/src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Register from './components/UserRegister';
-import PetRegister from './components/PetRegister';
-import UserList from './components/UserList';
+import Register from './pages/UserRegister';
+import PetRegister from './pages/PetRegister';
+import UserList from './pages/UserList';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/usuarios/register" element={<Register />} />
-        <Route path="/pets/petRegister" element={<PetRegister />} />
-        <Route path="/usuarios/" element={<UserList />} />
+          <Route path="/usuarios/register" element={<Register />} />
+          <Route path="/pets/petRegister" element={<PetRegister />} />
+          <Route path="/usuarios/" element={<UserList />} />
+        <Route element={<Layout />}>
+        </Route>
         {/* Você pode adicionar mais rotas aqui */}
       </Routes>
+
+      <h1>HOME</h1>
     </div>
   );
 }
